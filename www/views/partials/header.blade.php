b3345b3c5676ad6a2f6331af8c4303fdeb6597cb
user <dmitry.kulinichev@gmail.com>
2026-01-08 09:11:51 +0200

    поработал с контентом

--- www/views/partials/header.blade.php
+++ www/views/partials/header.blade.php
@@ -53,10 +53,11 @@
 
                 <!-- Кнопка входу в додаток -->
                 <a href="https://app.g24.com.ua/login" class="btn-login" target="_blank">
-                    <span>В гараж</span>
+                    <!-- Іконка входу (стрілка входить у двері) -->
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 1 18 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 12 21h6a2.25 2.25 0 0 0 2.25-2.25V15m-3 0 3-3m0 0-3-3m3 3H9" />
                     </svg>
+                    <span>В гараж</span>
                 </a>
             </div>
         </nav>
