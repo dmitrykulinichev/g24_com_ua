55962caf2cd1642a51bbd186762195b12dddcade
user <dmitry.kulinichev@gmail.com>
2026-01-28 16:35:36 +0200

    контент10

--- www/app/Services/Logger.php
+++ www/app/Services/Logger.php
@@ -3,7 +3,7 @@
 namespace App\Services;
 
 use Monolog\Logger as MonologLogger;
-use Monolog\Handler\StreamHandler;
+use Monolog\Handler\RotatingFileHandler;
 use Monolog\Formatter\LineFormatter;
 
 class Logger
@@ -17,14 +17,15 @@ class Logger
             self::$logger = new MonologLogger('app');
             
             $logFile = __DIR__ . '/../../storage/logs/app.log';
+            $maxFiles = (int)($_ENV['LOG_MAX_FILES'] ?? 14);
             
             // Налаштовуємо формат: [Дата] Канал.Рівень: Повідомлення {контекст} {додатково}
             $dateFormat = "Y-m-d H:i:s";
             $output = "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n";
             $formatter = new LineFormatter($output, $dateFormat);
 
-            // Створюємо хендлер (писати в файл)
-            $stream = new StreamHandler($logFile, MonologLogger::DEBUG);
+            // Створюємо хендлер з ротацією
+            $stream = new RotatingFileHandler($logFile, $maxFiles, MonologLogger::DEBUG);
             $stream->setFormatter($formatter);
 
             self::$logger->pushHandler($stream);
