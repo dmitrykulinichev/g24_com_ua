d8ead02ea254beedb0fdc770aa6e3e26d03149cb
user <dmitry.kulinichev@gmail.com>
2026-01-28 17:58:30 +0200

    контент12

--- www/views/landing.blade.php
+++ www/views/landing.blade.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="assets/css/style.css?v={{ time() }}">
     <!-- Підключення Tailwind CSS (через CDN для розробки) -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- AlpineJS для інтерактивності -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script>
         tailwind.config = {
             theme: {
