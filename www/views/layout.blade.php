d63d9294a29e507906e8c4b68a8f5b44f14326d4
dmitrykulinichev <dmitry.kulinichev@gmail.com>
2026-02-21 21:58:39 +0200

    контент2

--- www/views/layout.blade.php
+++ www/views/layout.blade.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
 <html lang="uk">
 <head>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-P5VR2VN5');</script>
-    <!-- End Google Tag Manager -->
+    @php
+        $googleAnalyticsId = getenv('GOOGLE_ANALYTICS_ID');
+    @endphp
+
+    @if($googleAnalyticsId)
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ $googleAnalyticsId }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+
+        // Google Consent Mode v2: Default state (Denied)
+        gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'ad_user_data': 'denied',
+            'ad_personalization': 'denied',
+            'analytics_storage': 'denied'
+        });
+
+        gtag('js', new Date());
+        gtag('config', '{{ $googleAnalyticsId }}');
+    </script>
+    @endif
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -60,11 +76,6 @@
     @stack('styles')
 </head>
 <body class="text-slate-800 antialiased bg-white" x-data="{ yearly: false }">
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5VR2VN5"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-
     @include('partials.header')
 
     <main>
